7f712689eb95b4484d991b2564eec0526ab507e1
cdswyda <cdswyda@hotmail.com>
2017-01-14 21:20:54 +0800

    update

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ console.log('start on port:' + port);
 /**
  * 路由配置
  */
+
 // index page
 app.get('/', function (req, res) {
     // 查询数据
@@ -51,35 +52,6 @@ app.get('/', function (req, res) {
 
     });
 
-
-    // res.render('index', {
-    //     title: '首页',
-    //     movies: [{
-    //         title: '机械战警',
-    //         _id: 1,
-    //         poster: 'https://baidu.com'
-    //     }, {
-    //         title: '机械战警',
-    //         _id: 2,
-    //         poster: 'https://baidu.com'
-    //     }, {
-    //         title: '机械战警',
-    //         _id: 3,
-    //         poster: 'https://baidu.com'
-    //     }, {
-    //         title: '机械战警',
-    //         _id: 4,
-    //         poster: 'https://baidu.com'
-    //     }, {
-    //         title: '机械战警',
-    //         _id: 5,
-    //         poster: 'https://baidu.com'
-    //     }, {
-    //         title: '机械战警',
-    //         _id: 6,
-    //         poster: 'https://baidu.com'
-    //     }]
-    // });
 });
 
 // detail page
@@ -94,6 +66,12 @@ app.get('/movie/:id', function (req, res) {
     });
 
 });
+
+// admin update movie
+app.get('/admin/update/:id',function(res,req){
+    
+});
+
 
 // admin post movie
 app.post('/admin/movie/new', function (res, req) {
@@ -133,16 +111,30 @@ app.post('/admin/movie/new', function (res, req) {
     });
 });
 
-// admin page
-app.get('/admin/movie', function (req, res) {
-    res.render('admin', {
-        title: '管理页'
-    });
-});
+
 
 // list page
 app.get('/admin/list', function (req, res) {
-    res.render('list', {
-        title: '列表页'
+    // 查询数据
+    Movie.fetch(function (err, movies) {
+        if (err) {
+            console.log(err);
+        }
+        res.render('list', {
+            title: '列表页',
+            movies: movies
+        });
+
+    });
+});
+
+
+
+// admin page
+app.get('/admin/movie', function (req, res) {   
+    
+    
+    res.render('admin', {
+        title: '管理页'
     });
 });
