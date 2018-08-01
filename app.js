abeab79acf6f7498cafe628851e1380890c7bf81
cdswyda <cdswyda@gamil.com>
2018-08-01 19:59:49 +0800

    cookie.secure must be false

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const session = require('express-session');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser'); express-session 1.5 之后就不依赖这个了
 // mongoose
 const mongoose = require('mongoose');
 // data
@@ -35,18 +35,24 @@ mongoose.connection.on('connected', function () {
 app.set('views', __dirname + '/views/pages');
 
 // 处理格式 解析json 解析表单等数据
-app.use(bodyParser.json()); // for parsing application/json
+// for parsing application/json
+app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({
+    // for parsing application/x-www-form-urlencoded
     extended: true
-})); // for parsing application/x-www-form-urlencoded
-
-app.use(cookieParser());
+})); 
+// Note Since version 1.5.0, the cookie-parser middleware no longer needs to be used for this module to work. This module now directly reads and writes cookies on req/res. Using cookie-parser may result in issues if the secret is not the same between this module and cookie-parser.
+// app.use(cookieParser());
 app.use(session({
     secret: 'learn node',
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure: true
+        // https://www.npmjs.com/package/express-session#cookiesecure
+        /*
+        Specifies the boolean value for the Secure Set-Cookie attribute. When truthy, the Secure attribute is set, otherwise it is not. By default, the Secure attribute is not set.
+        Note be careful when setting this to true, as compliant clients will not send the cookie back to the server in the future if the browser does not have an HTTPS connection.*/
+        secure: false
     }
 }));
 app.use(express.static(path.join(__dirname, 'public')));
